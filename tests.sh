d59a89032ec4c1b74d53d06240bb9cc146e6d944
bluss <bluss@users.noreply.github.com>
2023-06-10 18:18:09 +0200

    Remove old tox file

--- tests.sh
+++ tests.sh
@@ -4,9 +4,11 @@ set -ex
 : ${RYE_HOME:=$HOME/.rye}
 . "$RYE_HOME"/env
 
+# check that we can use rye discovery with virtualenv
+
 export VIRTUALENV_DISCOVERY=rye
 
-for version in cpython3.10 py39 3.9 py310 3.10 pypy39; do
+for version in py39 3.9 cpython3.10 py310 3.10 pypy39; do
     rye run virtualenv -p "$version" testenv/"$version"
 done
 
